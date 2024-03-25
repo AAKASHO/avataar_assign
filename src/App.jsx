3ea66f679f03a79b1f505d2dff186da6833598ef
aakasho <iamaakashkumar789@gmail.com>
2024-03-26 02:47:35 +0530

    speed up

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,6 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
 import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
-
-import slide_image_1 from './assets/images/img_1.png';
-import slide_image_2 from './assets/images/img_2.png';
-import slide_image_3 from './assets/images/img_3.png';
-import slide_image_4 from './assets/images/img_4.png';
-import slide_image_5 from './assets/images/img_5.png';
-import slide_image_6 from './assets/images/img_6.png';
-import slide_image_7 from './assets/images/img_7.png';
 import Navbar from './Navbar/Navbar';
 import Carousel from './components/Carousel/Carousel';
 import Slider from './Slider/Slider';
@@ -22,16 +14,6 @@ import Check from './Check/Check';
 
 
 function App() {
-
-  const [imgs,setImgs]=useState([
-    slide_image_1,
-    slide_image_2,
-    slide_image_3,
-    slide_image_4,
-    slide_image_5,
-    slide_image_6,
-    slide_image_7,
-  ])
   return (
     <div className='outsideContainer'>
     <Navbar className="appnav"/>
